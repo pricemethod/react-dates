3a1298112e26bc78d25304d8ab550a92f0cc012f
Greg Schechter <gregthebusker@gmail.com>
2018-10-04 14:22:45 -0700

    Align days of week

--- src/components/DayPicker.jsx
+++ src/components/DayPicker.jsx
@@ -1182,7 +1182,7 @@ export default withStyles(
 
     DayPicker_weekHeader_ul: {
       listStyle: 'none',
-      margin: '1px 0',
+      marginTop: 12,
       paddingLeft: 0,
       paddingRight: 0,
       fontSize: font.size,
